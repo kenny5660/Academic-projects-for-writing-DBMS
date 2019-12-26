8783f97056f67899dd26c67d3f2284fb786a4f44
Alkonior <sashadz2002@gmail.com>
2019-12-26 15:56:22 +1000

    Fix durability

--- IntegrationTests/DurabilityTest.cs
+++ IntegrationTests/DurabilityTest.cs
@@ -126,5 +126,109 @@ namespace IntegrationTests
             s2.Kill();
         }
 
+        [TestMethod]
+        public void DeleteDurabilityNoKill ()
+        {
+            DelFiles();
+            var _core = new DataBase(20, new DataBaseEngineMain(_testPath), new TransactionScheduler());
+            var expected = GetTestData();
+            var s1 = new TestApServer("s1", _core);
+            var cl1 = new TestClient("cl1", _core);
+            cl1.SendQuery("create table test (i int)");
+            for (var i = 0; i < 100; i++)
+            {
+                var res = cl1.SendQuery($"insert into test values ({i})");
+            }
+            var comand = "";
+            for (var i = 0; i < 100; i++)
+            {
+                comand += ($"delete from test where i = ({i});\n");
+            }
+            var transaction = "BEGIN TRANSACTION cl1\n" +
+                comand +
+                $"commit";
+
+            var t = Task.Run(() => { cl1.SendQuery(transaction); });
+
+            System.Threading.Thread.Sleep(100);
+            //s1.Kill();
+            System.Threading.Thread.Sleep(100);
+            t.Wait();
+            var s2 = new TestApServer("cl2", _core);
+            var cl2 = new TestClient("cl1", _core);
+
+            SendSQLQuery(cl2, "select * from test", expected);
+            s2.Kill();
+        }
+
+        [TestMethod]
+        public void InsertDurabilityNoKill ()
+        {
+            DelFiles();
+            var _core = new DataBase(20, new DataBaseEngineMain(_testPath), new TransactionScheduler());
+            var expected = GetTestData();
+            var s1 = new TestApServer("s1", _core);
+            var cl1 = new TestClient("cl1", _core);
+            cl1.SendQuery("create table test (i int)");
+            for (var i = 0; i < 100; i++)
+            {
+                var res = cl1.SendQuery($"insert into test values ({i})");
+            }
+            var comand = "";
+            for (var i = 0; i < 100; i++)
+            {
+                comand += ($"insert into test values ({i});\n");
+            }
+            var transaction = "BEGIN TRANSACTION cl1\n" +
+                comand +
+                $"commit";
+
+            var t = Task.Run(() => { cl1.SendQuery(transaction); });
+
+            System.Threading.Thread.Sleep(100);
+           // s1.Kill();
+            System.Threading.Thread.Sleep(100);
+            t.Wait();
+            var s2 = new TestApServer("cl2", _core);
+            var cl2 = new TestClient("cl1", _core);
+
+            SendSQLQuery(cl2, "select * from test", expected);
+            s2.Kill();
+        }
+
+        [TestMethod]
+        public void UpdateDurabilityNoKill ()
+        {
+            DelFiles();
+            var _core = new DataBase(20, new DataBaseEngineMain(_testPath), new TransactionScheduler());
+            var expected = GetTestData();
+            var s1 = new TestApServer("s1", _core);
+            var cl1 = new TestClient("cl1", _core);
+            cl1.SendQuery("create table test (i int)");
+            for (var i = 0; i < 100; i++)
+            {
+                var res = cl1.SendQuery($"insert into test values ({i})");
+            }
+            var comand = "";
+            for (var i = 0; i < 100; i++)
+            {
+                comand += ($"update test set i = 0 where i = ({i});\n");
+            }
+            var transaction = "BEGIN TRANSACTION cl1\n" +
+                comand +
+                $"commit";
+
+            var t = Task.Run(() => { cl1.SendQuery(transaction); });
+            System.Threading.Thread.Sleep(100);
+            // s1.Kill();
+            System.Threading.Thread.Sleep(100);
+            t.Wait();
+
+            var s2 = new TestApServer("cl2", _core);
+            var cl2 = new TestClient("cl1", _core);
+
+            SendSQLQuery(cl2, "select * from test", expected);
+            s2.Kill();
+        }
     }
 }
